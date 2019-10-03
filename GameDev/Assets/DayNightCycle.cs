8b097925b889c22ce9f74fcc5e395783c2f1376c
VinnyDolci <vinny.dolci@gmail.com>
2019-10-03 19:13:03 -0400

    Day Night
    Started wok on light cycle

---
+++ GameDev/Assets/DayNightCycle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public GameObject sun;
+    //Transform trans = sun.GetComponent<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
