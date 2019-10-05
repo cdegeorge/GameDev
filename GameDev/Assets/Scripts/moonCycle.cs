6d684bbf785421b961b35641a2ba6f96f4ec2bbb
VinnyDolci <vinny.dolci@gmail.com>
2019-10-04 23:39:58 -0400

    Built up terrain, finished day night cycle
    There's a small cube that doesn't mean much, just wanted to have something to move around.

--- GameDev/Assets/Scripts/moonCycle.cs
+++ GameDev/Assets/Scripts/moonCycle.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dayNightCycle : MonoBehaviour
+public class moonCycle : MonoBehaviour
 {
     new private Light light;
-    // Start is called before the first frame update
-    void Start()
-    {
-        light = GetComponent<Light>();   
+    
+    void Start() {
+        light = GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    
+    void Update() {
         transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);
         transform.LookAt(Vector3.zero);
         // Trees look weird when sun or moon is rising, especially the moon, trying to negate the effect.
-        if (transform.position.y < -20) {
+        if (transform.position.y < 200 && light.intensity > 0) {
             light.intensity -= .001f;
-        } else if (transform.position.y > -20 && light.intensity < .05f) {
+        }
+        else if (transform.position.y > -200 && light.intensity < .5f) {
             light.intensity += .001f;
         }
 
---
+++ GameDev/Assets/Scripts/rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    public GameObject target;           
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
