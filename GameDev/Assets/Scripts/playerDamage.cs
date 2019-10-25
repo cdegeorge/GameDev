25fa1744f074c73d24bb21c6fc2efca6d62345a4
VinnyDolci <vinny.dolci@gmail.com>
2019-10-24 22:01:44 -0400

    jump
    Jumping not working as thought

--- GameDev/Assets/Scripts/playerDamage.cs
+++ GameDev/Assets/Scripts/playerDamage.cs
@@ -26,7 +26,6 @@ public class playerDamage : MonoBehaviour
         timer += Time.deltaTime;
 
         if(Input.GetButton("Fire1") && timer >= timeBetweenHits) {
-            Debug.Log("Running");
             DoDamage();
         }
     }
--- GameDev/Assets/Scripts/sunCycle.cs
+++ GameDev/Assets/Scripts/sunCycle.cs
@@ -12,14 +12,14 @@ public class sunCycle : MonoBehaviour
 
     
     void Update() {
-        transform.RotateAround(Vector3.zero, Vector3.right, 1f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, 100f * Time.deltaTime);
         transform.LookAt(Vector3.zero);
         // Trees look weird when sun or moon is rising, especially the moon, trying to negate the effect.
-        if (transform.position.y < 200 && light.intensity > .031f) {
-            light.intensity -= .001f;
+        if (transform.position.y < 0 && light.intensity > .031f) {
+            light.intensity -= .01f;
         }
-        else if (transform.position.y > -200 && light.intensity < .5f) {
-            light.intensity += .001f;
+        else if (transform.position.y > 0 && light.intensity < .5f) {
+            light.intensity += .01f;
         }
 
     }
