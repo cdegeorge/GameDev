8793c4f70b035b11f602d1d8b768a0a35dbcddca
cdegeorge <connorjdeg@gmail.com>
2019-10-30 13:17:16 -0400

    Enemies Only Spawn at Night

--- GameDev/Assets/Scripts/SpawnManager.cs
+++ GameDev/Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     public float spawnTime = 3f;
 
     private PlayerHealth playerHealth;
+    private Transform sunPosition;
 
     private void Start()
     {
@@ -17,12 +18,14 @@ public class SpawnManager : MonoBehaviour
     private void Awake()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject sun = GameObject.Find("Sun");
         playerHealth = player.GetComponent<PlayerHealth>();
+        sunPosition = sun.transform;
     }
 
     private void Spawn()
     {
-        if (playerHealth.currentHealth > 0)
+        if (playerHealth.currentHealth > 0 && sunPosition.position.y <= 0)
             Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
     }
 }
