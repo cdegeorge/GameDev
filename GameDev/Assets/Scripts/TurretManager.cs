1b38688e1a23a5a326dd55e6bcafa2b1b0a8a97f
VinnyDolci <vinny.dolci@gmail.com>
2019-11-01 22:12:16 -0400

    Basic Turret
    Got a simple turret created. I don't think enemies are killable however... Also rock piles don't spawn on boulder destruction, and trees stopped disappearing.

---
+++ GameDev/Assets/Scripts/TurretManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour
+{
+    public int damagePerHit = 50;
+    public float timeBetweenHits = 1f;
+    public float range = 50f;
+    public GameObject barrel;
+
+    float timer;
+    Ray shootray;
+    RaycastHit shootHit;
+    int shootableMask;
+    LineRenderer hitLine;
+    Vector3 linePosition;
+
+    private void Awake() {
+        shootableMask = LayerMask.GetMask("Shootable");
+        hitLine = GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Enemy") {
+            transform.LookAt(other.transform);
+            Fire(other.transform);
+        }
+    }
+
+    void Fire (Transform target) {
+        timer = 0f;
+        linePosition = barrel.transform.position;
+        shootray.origin = linePosition;
+        shootray.direction = target.transform.position;
+
+        hitLine.enabled = true;
+        hitLine.SetPosition(0, linePosition);
+
+        if (Physics.Raycast(shootray, out shootHit, range, shootableMask)) {
+            damage damage = shootHit.collider.GetComponent<damage>();
+
+            if (damage != null) {
+                damage.TakeDamage(damagePerHit);
+                Debug.Log("Turret: Damage Sent");
+            }
+
+            hitLine.SetPosition(1, shootHit.point);
+        }
+    }
+}
